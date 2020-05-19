8563816f5971412c67192e869dadc831c4be8fd5
efsokli <efsokli@gmail.com>
2020-05-19 17:55:08 +0300

    enums

--- enums/src/main/java/enums/Main.java
+++ enums/src/main/java/enums/Main.java
@@ -19,14 +19,19 @@ public class Main {
         
         //Exercise 1
         System.out.print("What day is it today? ");
-        String answer=sc.nextLine().toUpperCase();
-        day.howManyDays(Friday.DaysOfWeek.valueOf(answer));
-        
+        while(sc.hasNextLine()){
+            String answer=sc.nextLine().toUpperCase();
+            day.howManyDays(Friday.DaysOfWeek.valueOf(answer));
+        }
         //Exercise 2
         System.out.print("Operand 1: ");
-        o1=Integer.parseInt(sc.nextLine());
+        while(sc.hasNextLine()){
+            o1=Integer.parseInt(sc.nextLine());
+        }
         System.out.print("Operand 2: ");
-        o2=Integer.parseInt(sc.nextLine());
+        while(sc.hasNextLine()){
+            o2=Integer.parseInt(sc.nextLine());
+        }
         operation.calculate(MathOperators.MathOperator.PLUS, o1, o2);
         operation.calculate(MathOperators.MathOperator.MINUS, o1, o2);
         operation.calculate(MathOperators.MathOperator.MULTIPLY, o1, o2);
